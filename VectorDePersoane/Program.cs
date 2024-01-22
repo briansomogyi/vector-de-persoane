53ba0626847d3a75b66d6a0435c8104f9c11d264
briansomogyi <brian.somogyi@emanuel.ro>
2024-01-22 08:46:32 +0200

    Implemented the Person Class

---
+++ VectorDePersoane/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace VectorDePersoane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of persons: ");
+            string? numberOfPersonsAsString = Console.ReadLine();
+            bool isOk = int.TryParse(numberOfPersonsAsString, out int numberOfPersons);
+            if (isOk) 
+            {
+                Person[] persons = new Person[numberOfPersons];
+                for (int i = 0; i < numberOfPersons; i++)
+                {
+                    Console.WriteLine($"Enter the data of Person[{i}]");
+                    Console.Write("Enter the first name: ");
+                    string? personFirstName = Console.ReadLine();
+                    Console.Write("Enter the last name: ");
+                    string? personLastName = Console.ReadLine();
+                    Console.Write("Enter the year of birth: ");
+                    string? yearOfBirthAsString = Console.ReadLine();
+                    bool isNumber = int.TryParse(yearOfBirthAsString, out int result);
+                    if (isNumber) 
+                    {
+                        persons[i] = new Person(personFirstName, personLastName, result);
+                    }
+                }
+            }
+            
+        }
+    }
+}
